6ab7803083090f33363b271c341547b88c089394
ATMcrazy8 <austin.t.mans@gmail.com>
2025-03-06 19:25:46 -0600

    build ready, es lint var ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }
+      ],
+      "react/no-unescaped-entities": "off" // Disables the 'unescaped entities' warning
+    },
+  },
 ];
 
 export default eslintConfig;
